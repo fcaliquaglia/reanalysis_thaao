3d41db6c685a6513f5f0287cb02fae13260c8c3d
Filippo Calì Quaglia <filippo.caliquaglia@gmail.com>
2025-03-26 13:41:08 +0100

    cleanup

--- read_all.py
+++ read_all.py
@@ -24,7 +24,6 @@ __lastupdate__ = ""
 from metpy.calc import wind_direction, wind_speed
 from metpy.units import units
 
-import inputs as inpt
 from read_reanalysis import *
 
 
@@ -51,16 +50,16 @@ def read():
 def read_all_alb():
     # CARRA
     read_reanalysis(inpt.var, 'c')
-    inpt.extr[inpt.var]['c']['data'] = inpt.extr[inpt.var]['c']['data'] / 100.
-    inpt.extr[inpt.var]['c']['data'][inpt.extr[inpt.var]['c']['data'] <= 0.] = np.nan
+    inpt.extr[inpt.var]['c']['data'] = inpt.extr[inpt.var]['c']['data'] / inpt.extr[inpt.var]['rescale_fact']
+    inpt.extr[inpt.var]['c']['data'][inpt.extr[inpt.var]['c']['data'] < inpt.extr[inpt.var]['excl_thresh']] = np.nan
 
     # ERA5
     read_reanalysis(inpt.var, 'e')
-    inpt.extr[inpt.var]['c']['data'][inpt.extr[inpt.var]['c']['data'] <= 0.] = np.nan
+    inpt.extr[inpt.var]['c']['data'][inpt.extr[inpt.var]['c']['data'] < inpt.extr[inpt.var]['excl_thresh']] = np.nan
 
     # ERA5-LAND
     read_reanalysis(inpt.var, 'l')
-    inpt.extr[inpt.var]['l']['data'][inpt.extr[inpt.var]['l']['data'] <= 0.] = np.nan
+    inpt.extr[inpt.var]['l']['data'][inpt.extr[inpt.var]['l']['data'] < inpt.extr[inpt.var]['excl_thresh']] = np.nan
 
     # THAAO
     read_thaao_rad(inpt.var)
@@ -71,16 +70,16 @@ def read_all_alb():
 def read_all_cbh():
     # CARRA
     read_reanalysis(inpt.var, 'c')
-    inpt.extr[inpt.var]['c']['data'][inpt.extr[inpt.var]['c']['data'] <= 20.] = np.nan
+    inpt.extr[inpt.var]['c']['data'][inpt.extr[inpt.var]['c']['data'] <= inpt.extr[inpt.var]['excl_thresh']] = np.nan
 
     # ERA5
     read_reanalysis(inpt.var, 'e')
-    inpt.extr[inpt.var]['e']['data'][inpt.extr[inpt.var]['e']['data'] <= 20.] = np.nan
+    inpt.extr[inpt.var]['e']['data'][inpt.extr[inpt.var]['e']['data'] <= inpt.extr[inpt.var]['excl_thresh']] = np.nan
     inpt.extr[inpt.var]['e']['data'] += inpt.thaao_elev
 
     # THAAO
     read_thaao_ceilometer(inpt.var)
-    inpt.extr[inpt.var]['t']['data'][inpt.extr[inpt.var]['t']['data'] <= 20.] = np.nan
+    inpt.extr[inpt.var]['t']['data'][inpt.extr[inpt.var]['t']['data'] <= inpt.extr[inpt.var]['excl_thresh']] = np.nan
 
     return
 
@@ -89,19 +88,16 @@ def read_all_lwp():
     # CARRA
     read_reanalysis(inpt.var, 'c')
     inpt.extr[inpt.var]['c']['data'] = inpt.extr[inpt.var]['c']['data']
-    inpt.extr[inpt.var]['c']['data'][inpt.extr[inpt.var]['c']['data'] < 0.01] = np.nan
-    # c[c < 15] = 0
+    inpt.extr[inpt.var]['c']['data'][inpt.extr[inpt.var]['c']['data'] < inpt.extr[inpt.var]['excl_thresh']] = np.nan
 
     # ERA5
     read_reanalysis(inpt.var, 'e')
     inpt.extr[inpt.var]['e']['data'] = inpt.extr[inpt.var]['e']['data']
-    inpt.extr[inpt.var]['e']['data'][inpt.extr[inpt.var]['e']['data'] < 0.01] = np.nan
-    # e[e < 15] = 0
+    inpt.extr[inpt.var]['e']['data'][inpt.extr[inpt.var]['e']['data'] < inpt.extr[inpt.var]['excl_thresh']] = np.nan
 
     # THAAO1
     read_thaao_hatpro(inpt.var)
-    inpt.extr[inpt.var]['t1']['data'][inpt.extr[inpt.var]['t1']['data'] < 0.01] = np.nan
-    # t1[t1 < 15] = 0
+    inpt.extr[inpt.var]['t1']['data'][inpt.extr[inpt.var]['t1']['data'] < inpt.extr[inpt.var]['excl_thresh']] = np.nan
 
     return
 
@@ -128,119 +124,123 @@ def read_all_precip():
 
 
 def read_all_lw_down():
+    vr = 'lw_down'
     # CARRA
-    read_reanalysis('lw_down', 'c')
-    inpt.extr['lw_down']['c']['data'][inpt.extr['lw_down']['c']['data'] < 0.] = np.nan
-    inpt.extr['lw_down']['c']['data'] = inpt.extr['lw_down']['c']['data'] / inpt.var_dict['c']['rad_conv_factor']
+    read_reanalysis(vr, 'c')
+    inpt.extr[vr]['c']['data'][inpt.extr[vr]['c']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
+    inpt.extr[vr]['c']['data'] = inpt.extr[vr]['c']['data'] / inpt.extr[vr]['rescale_factor']
 
     # ERA5
-    read_reanalysis('lw_down', 'e')
-    inpt.extr['lw_down']['e']['data'][inpt.extr['lw_down']['e']['data'] < 0.] = np.nan
-    inpt.extr['lw_down']['e']['data'] = inpt.extr['lw_down']['e']['data'] / inpt.var_dict['e']['rad_conv_factor']
+    read_reanalysis(vr, 'e')
+    inpt.extr[vr]['e']['data'][inpt.extr[vr]['e']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
+    inpt.extr[vr]['e']['data'] = inpt.extr[vr]['e']['data'] / inpt.extr[vr]['rescale_factor']
 
     # ERA5-LAND
-    read_reanalysis('lw_down', 'l')
-    inpt.extr['lw_down']['l']['data'][inpt.extr['lw_down']['l']['data'] < 0.] = np.nan
-    inpt.extr['lw_down']['l']['data'] = inpt.extr['lw_down']['l']['data'] / inpt.var_dict['l']['rad_conv_factor']
+    read_reanalysis(vr, 'l')
+    inpt.extr[vr]['l']['data'][inpt.extr[vr]['l']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
+    inpt.extr[vr]['l']['data'] = inpt.extr[vr]['l']['data'] / inpt.extr[vr]['rescale_factor']
 
     # THAAO
-    read_thaao_rad('lw_down')
-    inpt.extr['lw_down']['t']['data'][inpt.extr['lw_down']['t']['data'] < 0.] = np.nan
+    read_thaao_rad(vr)
+    inpt.extr[vr]['t']['data'][inpt.extr[vr]['t']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
     return
 
 
 def read_all_sw_down():
+    vr = 'sw_down'
     # CARRA
-    read_reanalysis('sw_down', 'c')
-    inpt.extr['sw_down']['c']['data'][inpt.extr['sw_down']['c']['data'] < 0.] = np.nan
-    inpt.extr['sw_down']['c']['data'] = inpt.extr['sw_down']['c']['data'] / inpt.var_dict['c']['rad_conv_factor']
+    read_reanalysis(vr, 'c')
+    inpt.extr[vr]['c']['data'][inpt.extr[vr]['c']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
+    inpt.extr[vr]['c']['data'] = inpt.extr[vr]['c']['data'] / inpt.extr[vr]['rescale_factor']
 
     # ERA5
-    read_reanalysis('sw_down', 'e')
-    inpt.extr['sw_down']['e']['data'][inpt.extr['sw_down']['e']['data'] < 0.] = np.nan
-    inpt.extr['sw_down']['e']['data'] = inpt.extr['sw_down']['e']['data'] / inpt.var_dict['e']['rad_conv_factor']
+    read_reanalysis(vr, 'e')
+    inpt.extr[vr]['e']['data'][inpt.extr[vr]['e']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
+    inpt.extr[vr]['e']['data'] = inpt.extr[vr]['e']['data'] / inpt.extr[vr]['rescale_factor']
 
     # ERA5-LAND
-    read_reanalysis('sw_down', 'l')
-    inpt.extr['sw_down']['l']['data'][inpt.extr['sw_down']['l']['data'] < 0.] = np.nan
-    inpt.extr['sw_down']['l']['data'] = inpt.extr['sw_down']['l']['data'] / inpt.var_dict['l']['rad_conv_factor']
+    read_reanalysis(vr, 'l')
+    inpt.extr[vr]['l']['data'][inpt.extr[vr]['l']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
+    inpt.extr[vr]['l']['data'] = inpt.extr[vr]['l']['data'] / inpt.extr[vr]['rescale_factor']
 
     # THAAO
-    read_thaao_rad('sw_down')
-    inpt.extr['sw_down']['t']['data'][inpt.extr['sw_down']['t']['data'] < 0.] = np.nan
+    read_thaao_rad(vr)
+    inpt.extr[vr]['t']['data'][inpt.extr[vr]['t']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
     return
 
 
 def read_all_lw_up():
+    vr = 'lw_up'
     read_all_lw_down()
 
     # CARRA
     read_reanalysis('lw_net', 'c')
-    inpt.extr['lw_net']['c']['data'] = inpt.extr['lw_net']['c']['data'] / inpt.var_dict['c']['rad_conv_factor']
-    inpt.extr['lw_up']['c']['data'] = pd.DataFrame(
+    inpt.extr['lw_net']['c']['data'] = inpt.extr['lw_net']['c']['data'] / inpt.extr[vr]['rescale_factor']
+    inpt.extr[vr]['c']['data'] = pd.DataFrame(
             index=inpt.extr['lw_down']['c']['data'].index,
-            data=inpt.extr['lw_down']['c']['data'].values - inpt.extr['lw_net']['c']['data'].values, columns=['lw_up'])
-    inpt.extr['lw_up']['c']['data'][inpt.extr['lw_up']['c']['data'] < 0.] = np.nan
+            data=inpt.extr['lw_down']['c']['data'].values - inpt.extr['lw_net']['c']['data'].values, columns=[vr])
+    inpt.extr[vr]['c']['data'][inpt.extr[vr]['c']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
     # del inpt.extr['lw_net']['c']['data']
 
     # ERA5
     read_reanalysis('lw_net', 'e')
-    inpt.extr['lw_net']['e']['data'] = inpt.extr['lw_net']['e']['data'] / inpt.var_dict['e']['rad_conv_factor']
-    inpt.extr['lw_up']['e']['data'] = pd.DataFrame(
+    inpt.extr['lw_net']['e']['data'] = inpt.extr['lw_net']['e']['data'] / inpt.extr[vr]['rescale_factor']
+    inpt.extr[vr]['e']['data'] = pd.DataFrame(
             index=inpt.extr['lw_down']['e']['data'].index,
-            data=inpt.extr['lw_down']['e']['data'].values - inpt.extr['lw_net']['e']['data'].values, columns=['lw_up'])
-    inpt.extr['lw_up']['e']['data'][inpt.extr['lw_up']['e']['data'] < 0.] = np.nan
+            data=inpt.extr['lw_down']['e']['data'].values - inpt.extr['lw_net']['e']['data'].values, columns=[vr])
+    inpt.extr[vr]['e']['data'][inpt.extr[vr]['e']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
     # del inpt.extr['lw_net']['e']['data']
 
     # ERA5-LAND
     read_reanalysis('lw_net', 'l')
-    inpt.extr['lw_net']['l']['data'] = inpt.extr['lw_net']['l']['data'] / inpt.var_dict['l']['rad_conv_factor']
-    inpt.extr['lw_up']['l']['data'] = pd.DataFrame(
+    inpt.extr['lw_net']['l']['data'] = inpt.extr['lw_net']['l']['data'] / inpt.extr[vr]['rescale_factor']
+    inpt.extr[vr]['l']['data'] = pd.DataFrame(
             index=inpt.extr['lw_down']['l']['data'].index,
-            data=inpt.extr['lw_down']['l']['data'].values - inpt.extr['lw_net']['l']['data'].values, columns=['lw_up'])
-    inpt.extr['lw_up']['l']['data'][inpt.extr['lw_up']['l']['data'] < 0.] = np.nan
+            data=inpt.extr['lw_down']['l']['data'].values - inpt.extr['lw_net']['l']['data'].values, columns=[vr])
+    inpt.extr[vr]['l']['data'][inpt.extr[vr]['l']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
     # del inpt.extr['lw_net']['l']['data']
 
     # THAAO
-    read_thaao_rad('lw_up')
-    inpt.extr['lw_up']['t']['data'][inpt.extr['lw_up']['t']['data'] < 0.] = np.nan
+    read_thaao_rad(vr)
+    inpt.extr[vr]['t']['data'][inpt.extr[vr]['t']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
 
     return
 
 
 def read_all_sw_up():
+    vr = 'sw_up'
     read_all_sw_down()
 
     # CARRA
     read_reanalysis('sw_net', 'c')
-    inpt.extr['sw_net']['c']['data'] = inpt.extr['sw_net']['c']['data'] / inpt.var_dict['c']['rad_conv_factor']
-    inpt.extr['sw_up']['c']['data'] = pd.DataFrame(
+    inpt.extr['sw_net']['c']['data'] = inpt.extr['sw_net']['c']['data'] / inpt.extr[vr]['rescale_factor']
+    inpt.extr[vr]['c']['data'] = pd.DataFrame(
             index=inpt.extr['sw_down']['c']['data'].index,
-            data=inpt.extr['sw_down']['c']['data'].values - inpt.extr['sw_net']['c']['data'].values, columns=['sw_up'])
-    inpt.extr['sw_up']['c']['data'][inpt.extr['sw_up']['c']['data'] < 0.] = np.nan
+            data=inpt.extr['sw_down']['c']['data'].values - inpt.extr['sw_net']['c']['data'].values, columns=[vr])
+    inpt.extr[vr]['c']['data'][inpt.extr[vr]['c']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
     del inpt.extr['sw_net']['c']['data']
 
     # ERA5
     read_reanalysis('sw_net', 'e')
-    inpt.extr['sw_net']['e']['data'] = inpt.extr['sw_net']['e']['data'] / inpt.var_dict['e']['rad_conv_factor']
-    inpt.extr['sw_up']['e']['data'] = pd.DataFrame(
+    inpt.extr['sw_net']['e']['data'] = inpt.extr['sw_net']['e']['data'] / inpt.extr[vr]['rescale_factor']
+    inpt.extr[vr]['e']['data'] = pd.DataFrame(
             index=inpt.extr['sw_down']['e']['data'].index,
-            data=inpt.extr['sw_down']['e']['data'].values - inpt.extr['sw_net']['e']['data'].values, columns=['sw_up'])
-    inpt.extr['sw_up']['e']['data'][inpt.extr['sw_up']['e']['data'] < 0.] = np.nan
+            data=inpt.extr['sw_down']['e']['data'].values - inpt.extr['sw_net']['e']['data'].values, columns=[vr])
+    inpt.extr[vr]['e']['data'][inpt.extr[vr]['e']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
     del inpt.extr['sw_net']['e']['data']
 
     # ERA5-LAND
     read_reanalysis('sw_net', 'l')
-    inpt.extr['sw_net']['l']['data'] = inpt.extr['sw_net']['l']['data'] / inpt.var_dict['l']['rad_conv_factor']
-    inpt.extr['sw_up']['l']['data'] = pd.DataFrame(
+    inpt.extr['sw_net']['l']['data'] = inpt.extr['sw_net']['l']['data'] / inpt.extr[vr]['rescale_factor']
+    inpt.extr[vr]['l']['data'] = pd.DataFrame(
             index=inpt.extr['sw_down']['l']['data'].index,
-            data=inpt.extr['sw_down']['l']['data'].values - inpt.extr['sw_net']['l']['data'].values, columns=['sw_up'])
-    inpt.extr['sw_up']['l']['data'][inpt.extr['sw_up']['l']['data'] < 0.] = np.nan
+            data=inpt.extr['sw_down']['l']['data'].values - inpt.extr['sw_net']['l']['data'].values, columns=[vr])
+    inpt.extr[vr]['l']['data'][inpt.extr[vr]['l']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
     del inpt.extr['sw_net']['l']['data']
 
     # THAAO
-    read_thaao_rad('sw_up')
-    inpt.extr['sw_up']['t']['data'][inpt.extr['sw_up']['t']['data'] < 0.] = np.nan
+    read_thaao_rad(vr)
+    inpt.extr[vr]['t']['data'][inpt.extr[vr]['t']['data'] < inpt.extr[vr]['excl_thresh']] = np.nan
 
     return
 
@@ -274,17 +274,17 @@ def read_all_rh():
 def read_all_surf_pres():
     # CARRA
     read_reanalysis(inpt.var, 'c')
-    inpt.extr[inpt.var]['c']['data'] = inpt.extr[inpt.var]['c']['data'] / 100.
-    inpt.extr[inpt.var]['c']['data'][inpt.extr[inpt.var]['c']['data'] <= 900] = np.nan
+    inpt.extr[inpt.var]['c']['data'] = inpt.extr[inpt.var]['c']['data'] / inpt.extr[inpt.var]['rescale_fact']
+    inpt.extr[inpt.var]['c']['data'][inpt.extr[inpt.var]['c']['data'] < inpt.extr[inpt.var]['excl_thresh']] = np.nan
 
     # ERA5
     read_reanalysis(inpt.var, 'e')
-    inpt.extr[inpt.var]['e']['data'] = inpt.extr[inpt.var]['e']['data'] / 100.
-    inpt.extr[inpt.var]['e']['data'][inpt.extr[inpt.var]['e']['data'] <= 900] = np.nan
+    inpt.extr[inpt.var]['e']['data'] = inpt.extr[inpt.var]['e']['data'] / inpt.extr[inpt.var]['rescale_fact']
+    inpt.extr[inpt.var]['e']['data'][inpt.extr[inpt.var]['e']['data'] < inpt.extr[inpt.var]['excl_thresh']] = np.nan
 
     # THAAO
     read_thaao_weather(inpt.var)
-    inpt.extr[inpt.var]['t']['data'][inpt.extr[inpt.var]['t']['data'] <= 900] = np.nan
+    inpt.extr[inpt.var]['t']['data'][inpt.extr[inpt.var]['t']['data'] < inpt.extr[inpt.var]['excl_thresh']] = np.nan
     inpt.extr[inpt.var]['t']['data'].loc['2021-10-11 00:00:00':'2021-10-19 00:00:00'] = np.nan
     inpt.extr[inpt.var]['t']['data'].loc['2024-4-26 00:00:00':'2024-5-4 00:00:00'] = np.nan
 
